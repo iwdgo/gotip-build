049907a0598ed79b0117d39a18afcfbd14e3a303
Constantin Konstantinidis <constantinkonstantinidis@gmail.com>
2024-09-10 16:33:18 +0200

    mips64le: remove set up of GOROOT

--- architecture/main.go
+++ architecture/main.go
@@ -150,7 +150,7 @@ func main() {
 	image := exec.Command("docker", "run", "-d", "-t",
 		"--platform", goarch,
 		// TODO GOROOT is arbitrary
-		"-e", setParam("GOROOT", "/tmp/go"),
+		// "-e", setParam("GOROOT", "/tmp/go"),
 		"-e", setParam("GO_TEST_TIMEOUT_SCALE", "4"),
 		"-e", setParam("GOPROXY", "https://proxy.golang.org,direct"),
 		"-e", setParam("GOSUMDB", "sum.golang.org"),
